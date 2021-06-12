872b5b5a0930b3176d32be2b67cc91b9741a4371
Semih Eroğlu <65253167+gears0000@users.noreply.github.com>
2021-06-12 18:15:41 +0300

    Create replikliste.js

---
+++ replikliste.js
@@ -0,0 +1,94 @@
+const replikliste = [
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286e83",
+    "yazi" : "Seni hiç sevmiyorum sütoğlan! Babanı da sevmezdim!"
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286e85",
+    "yazi" : "O nasıl soru memur bey. Biz sevişiyoruz."
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286e87",
+    "yazi" : "Reca ederim duygularımla oynamayın."
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286e89",
+    "yazi" : "Çocuklar, Mahmut Hoca iyileşmiş. dın dın dırırırın"
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286e8b",
+    "yazi" : "Sen mi büyüksün hayır ben büyüğüm ben! Yaşar Usta!"
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286e8d",
+    "yazi" : "Vücuduma sahip olabilirsin ama ruhuma asla!"
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286e8f",
+    "yazi" : "Amca, size baba diyebilir miyim?"
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286e91",
+    "yazi" : "- Sevgi neydi? - Sevgi emekti!"
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286e93",
+    "yazi" : "- Metiiin Metiiin goooş ev yanıyoo! - Nieeeee ev mi yaniyoor!.."
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286e95",
+    "yazi" : "Amca size baba diyebilir miyim ? "
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286e97",
+    "yazi" : "Biz ayrı dünyaların insanlarıyız"
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286e99",
+    "yazi" : "Bu da mı gol değil hakim bey?"
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286e9b",
+    "yazi" : "Durun siz evlenemezsiniz, çünkü kardeşsiniz"
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286e9d",
+    "yazi" : "Güzel olduğunuz kadar küstahsınız da"
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286e9f",
+    "yazi" : "Hatırlar mısınız, bir zamanlar fakir ama gururlu bir genç vardı"
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286ea1",
+    "yazi" : "n'ayır, n'olamaz"
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286ea3",
+    "yazi" : "Parayla saadet olmaz evladım, bunu sakın unutma!"
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286ea5",
+    "yazi" : "Pişman mısın yavrum ? "
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286ea7",
+    "yazi" : "Seni sevmiyorum, seninle oyun oynadım,  bunu anlamadın mı hala"
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286ea9",
+    "yazi" : "Senin annen bir melekti yavrum"
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286eab",
+    "yazi" : "Turist Ömer derler benim adıma, pişman olur bakmayanlar tadıma"
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286eaf",
+    "yazi" : "Sizde şiki baba kasedi var mı acaba ? "
+  },
+  {
+    "foto" : "https://i2.cnnturk.com/i/cnnturk/75/0x0/5273faf2050668163c286eb1",
+    "yazi" : "Lütfücüğüm herşeyi yapıyorum da bu gözlerimden ateş çıkaramıyorum"
+  }
+];
